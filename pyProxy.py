612ca5b88d367a03e9548c2d3f99c512e72d0c88
ycwan9 <ycwan9@outlook.com>
2015-11-10 19:32:48 +0800

    r0.1

--- pyProxy.py
+++ pyProxy.py
@@ -1,17 +1,22 @@
 # -*- coding: utf-8 -*-
 import BaseHTTPServer
+import SocketServer
 import proxy
+import socks
+class ThreadingHTTPServer (SocketServer.ThreadingMixIn, BaseHTTPServer.HTTPServer): pass
 
 if __name__ == '__main__':
     print 'hello'
     from sys import argv
-    if argv[2:] and argv[1] in ('-h', '--help'):
+    if len(argv)<2 or argv[1] in ('-h', '--help'):
         print argv[0], "socks_sever:port [port]"
     else:
+        socks_host, socks_port = argv[1].split(":")
+        socks_port = int(socks_port)
+        proxy.set_socks(socks.PROXY_TYPE_SOCKS5, socks_host, socks_port)
         port = 8080
-        if argv[1:]:
+        if argv[2:]:
             port = int(argv[1])
-        class ThreadingHTTPServer (SocketServer.ThreadingMixIn, BaseHTTPServer.HTTPServer): pass
         print "listen on %i"%port
         httpd = ThreadingHTTPServer(('0.0.0.0',port), proxy.proxy)
         try:
